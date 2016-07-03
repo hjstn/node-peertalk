6547c793775c13fed5586d944f26d759841c0ab7
justin97530 <jcraftr1@gmail.com>
2016-07-03 15:17:36 +0530

    Shortened index.js
    Remove unnecessary promise and variables.

--- index.js
+++ index.js
@@ -5,31 +5,22 @@ const bufferpack = require("bufferpack");
 
 function PeerTalk(options, port) {
     if(!port || typeof port !== "number") port = "2345";
-
-    return new Promise((resolve, reject) => {
         const event = new events.EventEmitter();
 
-        usbmux.getTunnel(port, options).then((tunnel) => {
-            resolve(event);
-
+        return usbmux.getTunnel(port, options).then((tunnel) => {
             event.on("send", (msg) => {
-                const enc = utf8.encode(msg),
-            	packed_data = bufferpack.pack("! I I I I", [1,101,0,enc.length+4]),
-            	packed_message = bufferpack.pack(`! I ${enc.length}s`, [enc.length, enc]);
-
-            	tunnel.write(packed_data);
-            	tunnel.write(packed_message);
+                msg = utf8.encode(msg);
+                tunnel.write(bufferpack.pack("! I I I I", [1,101,0,msg.length+4]));
+                tunnel.write(bufferpack.pack(`! I ${msg.length}s`, [msg.length, msg]));
             });
 
-        	tunnel.on("data", (data) => {
-        		const size = bufferpack.unpack("! I I I I", data)[3];
-        		const msg = data.slice(data.length - size).toString();
-                event.emit("data", msg);
-        	});
-        }).catch((err) => {
-            reject(err);
+            tunnel.on("data", (data) => {
+                const size = bufferpack.unpack("! I I I I", data)[3];
+                event.emit("data", data.slice(data.length - size).toString());
+            });
+
+            return event;
         });
-    });
 }
 
 module.exports = PeerTalk;
